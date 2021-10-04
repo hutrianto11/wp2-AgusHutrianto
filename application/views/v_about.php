87d827813ee83b260fa0f19515713d90c08c547e
hutrianto11 <hutrianto11@gmail.com>
2021-10-04 23:18:45 +0700

    Merancang Template sederhana dengan                 codeigniter

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Agus</li>
+        <li>Nama Belakang : Hutrianto</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li> Jakarta Utara</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Renang</li>
+    </ul>
+</section
